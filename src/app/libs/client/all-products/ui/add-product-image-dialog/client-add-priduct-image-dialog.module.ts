4f5a1c6acc6d7e32670201e7b23a57c33387aab8
destors <halen54001@gmail.com>
2022-12-15 17:23:20 +0200

    destors: product img btn minor fix

--- src/app/libs/client/all-products/ui/add-product-image-dialog/client-add-priduct-image-dialog.module.ts
+++ src/app/libs/client/all-products/ui/add-product-image-dialog/client-add-priduct-image-dialog.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientAddPriductImageDialogComponent } from './client-add-priduct-image-dialog.component';
 import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [ClientAddPriductImageDialogComponent],
-  imports: [CommonModule, DialogModule],
+  imports: [CommonModule, DialogModule, ButtonModule],
   exports: [ClientAddPriductImageDialogComponent],
 })
 export class ClientAddPriductImageDialogModule {}
