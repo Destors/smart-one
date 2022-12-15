813f601fb4d991d37faf4c7da4a85975edf337a4
destors <halen54001@gmail.com>
2022-12-15 13:06:34 +0200

    destors: products local storege basic btn

--- src/app/libs/client/all-products/ui/table/components/favorites-button/client-all-products-table-favorites-btn.component.ts
+++ src/app/libs/client/all-products/ui/table/components/favorites-button/client-all-products-table-favorites-btn.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { Product } from '../../../../common/product.interface';
 
 @Component({
@@ -9,12 +8,42 @@ import { Product } from '../../../../common/product.interface';
 })
 export class ClientAllProductsTableFavoritesBtnComponent implements OnInit {
   @Input() product!: Product;
-  form!: FormGroup;
-  constructor() {}
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      favorites: new FormControl(null),
-    });
+  productLocalState!: boolean;
+
+  ngOnInit() {
+    this.getProductLocalState(this.product.id!);
+  }
+
+  getProductLocalState(id: number) {
+    let localData = this.getData(id.toString());
+    if (localData) {
+      this.productLocalState = true;
+    } else this.productLocalState = false;
+  }
+
+  setProductLocalState() {
+    if (!this.productLocalState) {
+      this.removeData(this.product.id!.toString());
+      console.log('removed local');
+    } else {
+      this.saveData(
+        this.product.id!.toString(),
+        this.productLocalState.toString()
+      );
+      console.log('Add local');
+    }
+  }
+
+  public saveData(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public getData(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  public removeData(key: string) {
+    localStorage.removeItem(key);
   }
 }
