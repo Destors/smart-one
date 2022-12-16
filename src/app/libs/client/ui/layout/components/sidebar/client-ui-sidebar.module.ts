d99953fa7d2bac82296b2e8c94513b64fd684483
destors <halen54001@gmail.com>
2022-12-16 11:45:14 +0200

    destors: add core mixins scss

--- src/app/libs/client/ui/layout/components/sidebar/client-ui-sidebar.module.ts
+++ src/app/libs/client/ui/layout/components/sidebar/client-ui-sidebar.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientUiSidebarComponent } from './client-ui-sidebar.component';
 import { PanelMenuModule } from 'primeng/panelmenu';
-
+import { CardModule } from 'primeng/card';
 @NgModule({
   declarations: [ClientUiSidebarComponent],
-  imports: [CommonModule, PanelMenuModule],
+  imports: [CommonModule, PanelMenuModule, CardModule],
   exports: [ClientUiSidebarComponent],
 })
 export class ClientUiSidebarModule {}
