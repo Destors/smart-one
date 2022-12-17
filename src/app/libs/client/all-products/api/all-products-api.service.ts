af1e6a4005ab268d1cd7fa0a17d50ce5435dab02
destors <halen54001@gmail.com>
2022-12-17 20:59:04 +0200

    destors: api service, add handleError

--- src/app/libs/client/all-products/api/all-products-api.service.ts
+++ src/app/libs/client/all-products/api/all-products-api.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of, shareReplay } from 'rxjs';
+import {
+  catchError,
+  Observable,
+  of,
+  retry,
+  shareReplay,
+  throwError,
+} from 'rxjs';
 import {
   AddProductFormModel,
   Product,
@@ -22,13 +29,7 @@ export class AllProductsApiService {
       .get<ProductHttpResponse>(
         'https://backend-for-applicants.smartoneclub.com/products?limit=0&skip=0&ordering=id'
       )
-      .pipe(
-        shareReplay(1),
-        catchError((error: HttpErrorResponse) => {
-          console.error(error.message);
-          return (this.productsArr$ = of(undefined));
-        })
-      ));
+      .pipe(retry(3), shareReplay(1), catchError(this.handleError)));
   }
 
   public addProduct(form: AddProductFormModel) {
@@ -39,22 +40,23 @@ export class AllProductsApiService {
       brand: form.brand,
       category: form.category,
     };
-    return this.http.post<AddProductFormModel>(
-      'https://backend-for-applicants.smartoneclub.com/product',
-      body
-    );
+    return this.http
+      .post<AddProductFormModel>(
+        'https://backend-for-applicants.smartoneclub.com/product',
+        body
+      )
+      .pipe(catchError(this.handleError));
   }
 
   public addProductImage(productId: number, file: any) {
     const formData = new FormData();
     formData.append('file', file);
-    console.log(
-      `https://backend-for-applicants.smartoneclub.com/product/${productId}/image`
-    );
-    return this.http.post(
-      `https://backend-for-applicants.smartoneclub.com/product/${productId}/image`,
-      formData
-    );
+    return this.http
+      .post(
+        `https://backend-for-applicants.smartoneclub.com/product/${productId}/image`,
+        formData
+      )
+      .pipe(catchError(this.handleError));
   }
 
   // TODO Send only modified fields.
@@ -66,15 +68,37 @@ export class AllProductsApiService {
       brand: form.brand,
       category: form.category,
     };
-    return this.http.patch<Product>(
-      `https://backend-for-applicants.smartoneclub.com/product/${productId}`,
-      body
-    );
+    return this.http
+      .patch<Product>(
+        `https://backend-for-applicants.smartoneclub.com/product/${productId}`,
+        body
+      )
+      .pipe(catchError(this.handleError));
   }
 
   public delProduct(productId: number) {
-    return this.http.delete<Product>(
-      `https://backend-for-applicants.smartoneclub.com/product/${productId}`
+    return this.http
+      .delete<Product>(
+        `https://backend-for-applicants.smartoneclub.com/product/${productId}`
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened; please try again later.')
     );
   }
 }
