7da8c2b78af2e363d508d949434cfd5251ca2d95
destors <halen54001@gmail.com>
2022-12-17 14:22:38 +0200

    destors: move favorite table to ../

--- src/app/libs/client/selected-products/page/client-selected-products-page.module.ts
+++ src/app/libs/client/selected-products/page/client-selected-products-page.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientSelectedProductsPageRoutingModule } from './client-selected-products-page-routing.module';
 import { ClientSelectedProductsPageComponent } from './client-selected-products-page.component';
-import { ClientFavoriteProductTableModule } from '../ui/table/client-favorite-product-table/client-favorite-product-table.module';
-import { HttpClientModule } from '@angular/common/http';
 import { AllProductsApiService } from '../../all-products/api/all-products-api.service';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { CardModule } from 'primeng/card';
+import { ClientFavoriteProductTableModule } from '../ui/table/client-favorite-product-table.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ClientSelectedProductsPageComponent],
